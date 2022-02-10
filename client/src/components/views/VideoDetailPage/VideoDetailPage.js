994de87c9f753cbe803be124c454815bd8405785
divertome <hans9645@gmail.com>
2022-02-10 22:26:14 +0900

    videoDetail page title add

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, List, Avatar } from "antd";
+import { Row, Col, List, Avatar, Card, Typography } from "antd";
 import Axios from "axios";
 import SideVideo from "./Sections/SideVideo";
 import Subscribe from "./Sections/Subscribe";
@@ -11,6 +11,8 @@ function VideoDetailPage(props) {
   const variable = { videoId: videoId };
   const [VideoDetail, setVideoDetail] = useState([]);
   const [Comments, setComments] = useState([]);
+  const { Meta } = Card;
+  const { Title } = Typography;
 
   useEffect(() => {
     Axios.post("/api/video/getVideoDetail", variable).then((response) => {
@@ -62,8 +64,15 @@ function VideoDetailPage(props) {
               ]}
             >
               <List.Item.Meta
-                avatar={<Avatar src={VideoDetail.writer.image} />}
-                title={VideoDetail.writer.name}
+                avatar={
+                  <Meta
+                    bordered
+                    size="small"
+                    avatar={<Avatar src={VideoDetail.writer.image} />}
+                    title={VideoDetail.writer.name}
+                  />
+                }
+                title={<Title level={3}>{VideoDetail.title} </Title>}
                 description={VideoDetail.description}
               ></List.Item.Meta>
             </List.Item>
